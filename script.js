d325ad391e2bb6bd493eb22dd6a7f0a5088f224b
idanygo <ida.nygren@hotmail.com>
2023-02-16 15:15:08 +0100

    added input field

--- script.js
+++ script.js
@@ -11,31 +11,39 @@
 //     "userName": "string"
 //   }
 
-
+const submit = document.getElementById("btn-send");
+const userName = document.getElementById("username");
+const message = document.getElementById("message");
 
 function postData(username, message) {
-
+  fetch("http://localhost:3030/", {
+    method: "POST",
+    body: JSON.stringify({
+      msg: message,
+      userName: username,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
 
 function getData() {
-
+  fetch("http://localhost:3030/")
+    .then((response) => response.json())
+    .then((data) => {
+      const newListItem = document.createElement("p");
+      newListItem.innerText = "test";
+    });
 }
 
+function onSubmitClick(event) {
+  event.preventDefault();
+  postData(userName.value, message.value);
+  getData();
+}
 
-
-
-fetch("http://localhost:3030/")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-
-
-fetch("http://localhost:3030/", {
-  method: "POST",
-  body: JSON.stringify({
-    msg: "Vad heter du",
-    userName: "El"
-  }),
-  headers: { "Content-type": "application/json;  charset=UTF-8", "accept": "application/json" }
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+submit.addEventListener("click", onSubmitClick);
