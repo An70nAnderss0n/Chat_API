a78dc92e40b3363adf835f5c2402ae68400615a9
Anton Andersson <anton.andersson@brightstraining.com>
2023-02-16 14:38:57 +0100

    second commit

--- script.js
+++ script.js
@@ -11,6 +11,31 @@
 //     "userName": "string"
 //   }
 
+
+
+function postData(username, message) {
+
+}
+
+function getData() {
+
+}
+
+
+
+
 fetch("http://localhost:3030/")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+
+fetch("http://localhost:3030/", {
+  method: "POST",
+  body: JSON.stringify({
+    msg: "Vad heter du",
+    userName: "El"
+  }),
+  headers: { "Content-type": "application/json;  charset=UTF-8", "accept": "application/json" }
+})
   .then((response) => response.json())
   .then((data) => console.log(data));
